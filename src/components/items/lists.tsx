8f77f3912bcb0ee254ae6943fbb5095072849ccb
Manuel <manueljoseph113@gmail.com>
2020-09-26 21:24:35 -0400

    added bullet list

---
+++ src/components/items/lists.tsx
@@ -0,0 +1,19 @@
+import {makeStyles} from '@material-ui/core/styles'
+
+const useBulletListStyles = makeStyles(theme => ({
+    root: {
+        '& li': {
+            paddingBottom: theme.spacing(1)
+        }
+    }
+}))
+
+export function BulletList({children}) {
+
+    const classes = useBulletListStyles()
+    return (
+        <ul className={classes.root}>
+            {children}
+        </ul>
+    )
+}
