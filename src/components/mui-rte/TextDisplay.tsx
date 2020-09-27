b1c7fa279424067ed06e4904f73664fad74a367e
Manuel <manueljoseph113@gmail.com>
2020-09-27 17:28:18 -0400

    added mui-rte text display compononent

---
+++ src/components/mui-rte/TextDisplay.tsx
@@ -0,0 +1,20 @@
+import MUIRichTextEditor from 'mui-rte'
+import {Box} from '@material-ui/core'
+import {useState, useEffect} from 'react'
+
+interface Props {
+    text: string;
+}
+
+export default function TextDisplay({text}:Props) {
+
+    const [clientSide, setClientSide] = useState(false)
+
+    useEffect(() => setClientSide(true), [])
+
+    return (
+        <Box>
+            {clientSide && <MUIRichTextEditor defaultValue={text} controls={[]} readOnly /> }
+        </Box>
+    )
+}
