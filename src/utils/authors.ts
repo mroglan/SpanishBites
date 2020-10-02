c1e78346da6b104e0b4555ff24d6ffaf4e2d49e7
Manuel <manueljoseph113@gmail.com>
2020-10-02 15:05:43 -0400

    function for getting indiv author

---
+++ src/utils/authors.ts
@@ -0,0 +1,36 @@
+import database from '../database/database'
+import {DBAuthor} from '../database/dbInterfaces'
+import {ObjectId} from 'mongodb'
+
+export const getAllAuthors = async () => {
+    const db = await database()
+
+    const authors:DBAuthor[] = await db.collection('authors').aggregate([
+        {$lookup: {
+            from: 'timePeriods',
+            localField: 'timePeriod',
+            foreignField: '_id',
+            as: 'timePeriod'
+        }},
+        {$unwind: '$timePeriod'}
+    ]).sort({'lastName': 1}).toArray()
+    
+    return authors
+}
+
+export const getAuthor = async (id:ObjectId) => {
+    const db = await database()
+
+    const author:DBAuthor[] = await db.collection('authors').aggregate([
+        {$match: {'_id': id}},
+        {$lookup: {
+            from: 'timePeriods',
+            localField: 'timePeriod',
+            foreignField: '_id',
+            as: 'timePeriod'
+        }},
+        {$unwind: '$timePeriod'}
+    ]).toArray()
+
+    return author[0]
+}
