1758bb7fb527137fd6f16efb0ca7fd373c4d3156
Manuel <manueljoseph113@gmail.com>
2020-10-02 15:02:44 -0400

    added api route for indiv book

---
+++ src/pages/api/books/[id].tsx
@@ -0,0 +1,24 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import {getBook} from '../../../utils/books'
+import {ObjectId} from 'mongodb'
+
+export default async function GetBook(req:NextApiRequest, res:NextApiResponse) {
+
+    const id = req.query.id as string
+    
+    try {   
+        if(!ObjectId.isValid(id)) {
+            return res.status(400).json({msg: 'Invalid ObjectId'})
+        }
+    
+        if(req.method === 'GET') {
+            const book = await getBook(new ObjectId(id))
+            return res.status(200).json({book})
+        }
+        
+        return res.status(400).json({msg: 'Invalid request'})
+    } catch(e) {
+        return res.status(500).json({msg: 'Internal server error'})
+    }
+    
+}
