2899fe3653d41d8739ff1c0d514e1ca7908bb698
Manuel <manueljoseph113@gmail.com>
2020-10-02 22:03:56 -0400

    added api route for all authors

---
+++ src/pages/api/authors/index.ts
@@ -0,0 +1,17 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import {getAllAuthors} from '../../../utils/authors'
+import {ObjectId} from 'mongodb'
+
+export default async function GetAllAuthors(req:NextApiRequest, res:NextApiResponse) {
+
+    try {
+        if(req.method === 'GET') {
+            const authors = await getAllAuthors()
+            return res.status(200).json({authors})
+        }
+
+        return res.status(400).json({msg: 'Invalid request'})
+    } catch(e) {
+        return res.status(500).json({msg: 'Internal Server Error'})
+    }
+}
