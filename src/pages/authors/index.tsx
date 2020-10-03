dbb27f8870c85a6f93062d18a8a2304771f312c1
Manuel <manueljoseph113@gmail.com>
2020-10-02 22:04:17 -0400

    added authors index page

--- src/pages/authors/index.tsx
+++ src/pages/authors/index.tsx
@@ -2,18 +2,20 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import database from '../../database/database'
 import {DBAuthor, ClientAuthor} from '../../database/dbInterfaces'
 import {getAllAuthors} from '../../utils/authors'
-import {ObjectId} from 'mongodb'
 import styles from '../../styles/ResourceList.module.css'
 import Head from 'next/head'
 import Link from 'next/link'
 import MainHeader from '../../components/nav/MainHeader'
 import useSWR from 'swr'
+import Main from '../../components/authors/indexPage/Main'
 
 interface Props {
     authors: ClientAuthor[];
 }
 
 export default function Authors({authors:dbAuthors}:Props) {
+
+    const {data: {authors}} = useSWR(`/api/authors`, {initialData: {authors: dbAuthors}})
 
     return (
         <>
@@ -25,7 +27,7 @@ export default function Authors({authors:dbAuthors}:Props) {
                     <MainHeader />
                 </div>
                 <div>
-                    display authors here.
+                    <Main authors={authors} />
                 </div>
                 <div>
                     footer
@@ -34,16 +36,6 @@ export default function Authors({authors:dbAuthors}:Props) {
         </>
     )
 }
-
-/*
-{authors.map(author => (
-                    <Link key={author._id} href="/authors/[id]" as={`/authors/${author._id}`}>
-                        <a>
-                            {author.firstName} {author.lastName}<br />
-                        </a>
-                    </Link>
-                ))}
-*/
 
 export const getStaticProps:GetStaticProps = async () => {
 
