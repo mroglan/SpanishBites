d7712744eb4ce7e048094145d32cf9425f4765c6
Manuel <manueljoseph113@gmail.com>
2020-10-10 18:16:35 -0400

    created resource not found default page

---
+++ src/components/error/ResourceNotFound.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function ResourceNotFound() {
+
+    return (
+        <>
+            <Head>
+                <title>Resource not found!</title>
+            </Head>
+            <div>
+                That's All Folks!
+            </div>
+        </>
+    )
+}
