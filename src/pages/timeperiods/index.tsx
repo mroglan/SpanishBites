b9fcf1b2f0167c91c4cd9c9043d7ec699c97899e
Manuel <manueljoseph113@gmail.com>
2020-11-27 20:22:50 -0500

    passed unpopulated authors

--- src/pages/timeperiods/index.tsx
+++ src/pages/timeperiods/index.tsx
@@ -1,19 +1,23 @@
 import {GetStaticProps} from 'next'
-import {ClientTimePeriod} from '../../database/dbInterfaces'
+import {ClientTimePeriod, ClientAuthor, ClientUnpopulatedAuthor} from '../../database/dbInterfaces'
 import useSWR from 'swr'
 import Head from 'next/head'
 import styles from '../../styles/ResourceList.module.css'
 import MainHeader from '../../components/nav/MainHeader'
 import Main from '../../components/timePeriods/indexPage/Main'
 import { getAllTimePeriods } from '../../utils/timePeriods'
+import {getAllAuthors, getAllUnpopulatedAuthors} from '../../utils/authors'
 
 interface Props {
     timePeriods: ClientTimePeriod[];
+    authors: ClientUnpopulatedAuthor[];
 }
 
-export default function TimePeriods({timePeriods:dbTimePeriods}:Props) {
+export default function TimePeriods({timePeriods:dbTimePeriods, authors:dbAuthors}:Props) {
 
     const {data: {timePeriods}} = useSWR('/api/timeperiods', {initialData: {timePeriods: dbTimePeriods}})
+
+    const {data: {authors}} = useSWR('/api/authors/unpopulated', {initialData: {authors:dbAuthors}})
 
     return (
         <>
@@ -25,7 +29,7 @@ export default function TimePeriods({timePeriods:dbTimePeriods}:Props) {
                     <MainHeader />
                 </div>
                 <div>
-                    <Main timePeriods={timePeriods} />
+                    <Main timePeriods={timePeriods} authors={authors} />
                 </div>
                 <div>
                     footer
@@ -37,7 +41,7 @@ export default function TimePeriods({timePeriods:dbTimePeriods}:Props) {
 
 export const getStaticProps:GetStaticProps = async () => {
 
-    const timePeriods = await getAllTimePeriods()
+    const [timePeriods, authors] = await Promise.all([getAllTimePeriods(), getAllUnpopulatedAuthors()])
 
-    return {props: {timePeriods: JSON.parse(JSON.stringify(timePeriods))}, revalidate: 60}
+    return {props: {timePeriods: JSON.parse(JSON.stringify(timePeriods)), authors: JSON.parse(JSON.stringify(authors))}, revalidate: 60}
 }
