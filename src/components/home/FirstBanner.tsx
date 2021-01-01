2dfdf8e290d6cf6003915b853af85b1f97af3ad8
Manuel <manueljoseph113@gmail.com>
2021-01-01 17:06:51 -0500

    adjusted for smaller screen sizes

--- src/components/home/FirstBanner.tsx
+++ src/components/home/FirstBanner.tsx
@@ -1,45 +1,56 @@
 import styles from '../../styles/Home.module.css'
-import {Box, Typography, Grid} from '@material-ui/core'
+import {Box, Typography, Grid, useMediaQuery} from '@material-ui/core'
 import {RedPrimaryButton, RedSecondaryButton} from '../items/buttons'
+
+const bannerImage = (mobile:boolean, small:boolean) => {
+    if(mobile) return '/landingPage/donQuijote4.png'
+    if(small) return '/landingPage/donQuijote3.png'
+    return '/landingPage/donQuijote2.png'
+}
 
 export default function Banner() {
 
+    const smallScreen = useMediaQuery('(max-width:1200px)')
+    const mobileScreen = useMediaQuery('(max-width: 750px)');
+
     return (
         <div className={styles['donQuijote-banner-root']}>
-            <div className={styles['main-message-container']}>
-                <Box>
-                    <Box textAlign="center">
-                        <Typography variant="h2" component="h1" className={`${styles.catamaran} ${styles['red']}`} gutterBottom>
-                            Bite Size Spanish Literature
-                        </Typography>
-                    </Box>
-                    <Box textAlign="center" px={6}>
-                        <Typography variant="h6" className={`${styles['light-red']}`}>
-                            Immerse yourself in Spanish Literature by browsing a collection of authors, books, passages, and more.
-                        </Typography>
-                    </Box>
-                    <Box mt={3}>
-                        <Grid container spacing={3} justify="center">
-                            <Grid item>
-                                <RedPrimaryButton variant="contained">
-                                    <Typography variant="body1" color="inherit">
-                                        Create an Account
-                                    </Typography>
-                                </RedPrimaryButton>
+            <div>
+                <div className={styles['main-message-container']}>
+                    <Box my={4}>
+                        <Box className={styles['main-message-textbox']}>
+                            <Typography variant="h2" component="h1" className={`${styles.catamaran} ${styles['red']} ${styles['main-message-title']}`} gutterBottom>
+                                Bite Size Spanish Literature
+                            </Typography>
+                        </Box>
+                        <Box className={styles['main-message-textbox']} px={6}>
+                            <Typography variant="h6" className={`${styles['light-red']}`}>
+                                Immerse yourself in Spanish Literature by browsing a collection of authors, books, passages, and more.
+                            </Typography>
+                        </Box>
+                        <Box mt={3}>
+                            <Grid container spacing={3} justify="center">
+                                <Grid item>
+                                    <RedPrimaryButton variant="contained">
+                                        <Typography variant="body1" color="inherit">
+                                            Create an Account
+                                        </Typography>
+                                    </RedPrimaryButton>
+                                </Grid>
+                                <Grid item>
+                                    <RedSecondaryButton variant="outlined">
+                                        <Typography variant="body1" >
+                                            Learn More
+                                        </Typography>
+                                    </RedSecondaryButton>
+                                </Grid>
                             </Grid>
-                            <Grid item>
-                                <RedSecondaryButton variant="outlined">
-                                    <Typography variant="body1" >
-                                        Learn More
-                                    </Typography>
-                                </RedSecondaryButton>
-                            </Grid>
-                        </Grid>
+                        </Box>
                     </Box>
-                </Box>
-            </div>
-            <div className={styles['donQuijote-image']}>
-                <img src="/landingPage/donQuijote2.png" title="Don Quijote" />
+                </div>
+                <div className={styles['donQuijote-image']}>
+                    <img src={bannerImage(mobileScreen, smallScreen)} title="Don Quijote" />
+                </div>
             </div>
         </div>
     )
