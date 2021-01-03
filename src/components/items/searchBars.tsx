f1bca1560f6bd1ee6236f6363366a04c49d6cf2e
Manuel <manueljoseph113@gmail.com>
2021-01-03 18:28:07 -0500

    added testids

--- src/components/items/searchBars.tsx
+++ src/components/items/searchBars.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {FormControl, OutlinedInput, InputAdornment, IconButton} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import SearchIcon from '@material-ui/icons/Search'
@@ -38,16 +39,17 @@ export function PrimarySearchBar({search, setSearch}:Props) {
     return (
         <FormControl variant="outlined">
             <OutlinedInput id="search-bar" placeholder="Search..." onKeyPress={handleKeyPress} onBlur={handleBlur}
-            value={input} onChange={(e) => setInput(e.target.value as string)} margin="dense"
+            value={input} onChange={(e) => setInput(e.target.value as string)} margin="dense" inputProps={{'data-testid': 'input'}}
             startAdornment={
                 <InputAdornment position="start">
-                    <IconButton aria-label="Search" className={classes.button} onClick={() => setSearch(input)} edge="start" ref={searchRef}>
+                    <IconButton aria-label="Search" data-testid="searchBtn" className={classes.button} onClick={() => setSearch(input)} 
+                    edge="start" ref={searchRef}>
                         <SearchIcon />
                     </IconButton>
                 </InputAdornment>
             } endAdornment={
                 Boolean(input) && <InputAdornment position="end">
-                    <IconButton aria-label="Clear Search" className={classes.button} onClick={() => {
+                    <IconButton aria-label="Clear Search" data-testid="clearBtn" className={classes.button} onClick={() => {
                         setInput('')
                         setSearch('')
                     }} edge="end">
