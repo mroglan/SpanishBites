8349f23c4bce002c73df37ce6364bede61897f19
Manuel <manueljoseph113@gmail.com>
2021-01-03 19:05:43 -0500

    added test for authors filter

---
+++ src/__tests__/unit/components/library/authors/indexPage/Filters.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {mount} from 'enzyme'
+
+import Filters from '../../../../../../components/library/authors/indexPage/Filters'
+
+interface DispatchVal {
+    type: string;
+    payload: string;
+}
+
+describe('Authors Filters', () => {
+
+    const filters = {search: '', sort: 'lastName', mode: 'detailed'}
+    const dispatch = jest.fn((val:DispatchVal) => val)
+
+    const wrapper = mount(<Filters filters={filters} dispatch={dispatch} />)
+
+    it('Updates the search', () => {
+        wrapper.find('[data-testid="searchInput"]').simulate('change', {target: {value: 'hello world'}})
+        expect(dispatch).toHaveBeenCalled()
+    })
+
+    it('Updates the sort mode', () => {
+        wrapper.find('[data-testid="authorSortInput"]').filter('input').simulate('change', {target: {value: 'firstName'}})
+        expect(dispatch).toHaveBeenCalledWith({type: 'CHANGE_SORT', payload: 'firstName'})
+    })
+
+    it('Updates the viewing mode', () => {
+        wrapper.find('[data-testid="authorModeInput"]').filter('input').simulate('change', {target: {value: 'list'}})
+        expect(dispatch).toHaveBeenCalledWith({type: 'CHANGE_MODE', payload: 'list'})
+    })
+
+})
