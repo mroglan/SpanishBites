629893f691707868929cc72d34148904e47a6031
Manuel <manueljoseph113@gmail.com>
2021-01-05 17:38:20 -0500

    added testids

--- src/components/library/timePeriods/TimeLine.tsx
+++ src/components/library/timePeriods/TimeLine.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {Event as dbEvent} from '../../../database/dbInterfaces'
 import styles from '../../../styles/Timeline.module.css'
 import {Box, Grid, Typography} from '@material-ui/core'
@@ -18,11 +19,11 @@ export default function TimeLine({events}:Props) {
     return (
         <div className={styles.timeline}>
             {events.map((event, i) => (
-                <div key={i} className={`${styles.container} ${styles[event.side]}`}>
+                <div key={i} data-testid="eventItem" className={`${styles.container} ${styles[event.side]}`}>
                     <div className={styles.content}>
                        <div>
                             <Box>
-                                <Typography variant="h4">
+                                <Typography data-testid="eventDate" variant="h4">
                                     {event.date}
                                 </Typography>
                             </Box>
@@ -30,18 +31,20 @@ export default function TimeLine({events}:Props) {
                                 <Grid container alignItems="center">
                                     <LocationOnIcon color="secondary" fontSize="inherit" />
                                     <Grid item>
-                                        <Typography  variant="body1">
+                                        <Typography data-testid="eventLocation"  variant="body1">
                                             <em>{event.location}</em>
                                         </Typography>
                                     </Grid>
                                 </Grid>
                             </Box>}
                             <Box>
-                                <TextDisplay text={event.desc} />
+                                <div data-testid="eventDescription">
+                                    <TextDisplay text={event.desc} />
+                                </div>
                             </Box>
                         </div>
                         <div className={styles['img-container']}>
-                            <img src={event.image || '/no-profile.jpg'} title={event.title} />
+                            <img data-testid="eventImage" src={event.image || '/no-profile.jpg'} title={event.title} />
                         </div> 
                     </div>
                 </div>
