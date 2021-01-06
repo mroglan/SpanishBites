943833e7783249e6f001e4e63ec462ae7b1d1ee3
Manuel <manueljoseph113@gmail.com>
2021-01-06 10:12:16 -0500

    added unit tests

---
+++ src/__tests__/unit/components/library/timePeriods/indexPage/List.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import List from '../../../../../../components/library/timePeriods/indexPage/List'
+
+describe('TimePeriods Index Page List', () => {
+
+    const timePeriods = [
+        {_id: '', name: '', dateRange: [], intro: '', spainEvents: [], worldEvents: []},
+        {_id: '', name: '', dateRange: [], intro: '', spainEvents: [], worldEvents: []}
+    ]
+    const authors = [[{_id: '1', firstName: 'test', lastName: 'a', keyPoints: [''], relevantWorks: [], birthDate: '', deathDate: '', image: '', detailedInfo: '',
+    timePeriod: ''}], []]
+
+    const wrapper = shallow(<List timePeriods={timePeriods} authors={authors} />)
+
+    it('Lists all period names', () => {
+        expect(wrapper.find('[data-testid="periodListItemName"]').length).toEqual(2)
+    })
+
+    it('Displays all period intros', () => {
+        expect(wrapper.find('[data-testid="periodListItemIntro"]').length).toEqual(2)
+    })
+})
