d6ce09254eea363832b6c3b3e3f6fbaf26cbbf2c
Manuel <manueljoseph113@gmail.com>
2021-01-07 09:03:15 -0500

    added blue primary and secondary buttons

--- src/components/items/buttons.tsx
+++ src/components/items/buttons.tsx
@@ -1,6 +1,19 @@
 import {withStyles} from '@material-ui/core/styles'
 import {Button} from '@material-ui/core'
 
+export const BluePrimaryButton = withStyles(theme => ({
+    root: {
+        background: theme.palette.primary.main,
+        color: '#fff', 
+        borderRadius: theme.spacing(3),
+        padding: `${theme.spacing(2)}px ${theme.spacing(3)}px`,
+        transition: 'background 300ms', 
+        '&:hover': {
+            background: theme.palette.primary.dark
+        }
+    }
+}))(Button)
+
 export const RedPrimaryButton = withStyles(theme => ({
     root: {
         background: theme.palette.secondary.main,
@@ -9,7 +22,23 @@ export const RedPrimaryButton = withStyles(theme => ({
         padding: `${theme.spacing(2)}px ${theme.spacing(3)}px`,
         transition: 'background 300ms',
         '&:hover': {
-            background: theme.palette.secondary.dark //'hsl(359, 94%, 26%)'
+            background: theme.palette.secondary.dark
+        }
+    }
+}))(Button)
+
+export const BlueSecondaryButton = withStyles(theme => ({
+    root: {
+        background: 'transparent',
+        border: `2px solid #000`,
+        borderRadius: theme.spacing(3),
+        padding: `${theme.spacing(2)}px ${theme.spacing(3)}px`,
+        //color: 'hsl(359, 80%, 50%)',
+        transition: 'background color border 300ms',
+        '&:hover': {
+            color: '#fff',
+            background: theme.palette.primary.dark,
+            borderColor: theme.palette.primary.dark
         }
     }
 }))(Button)
