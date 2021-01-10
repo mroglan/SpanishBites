db928fc5b3d1785159fb021b9100bfdd3dd7d171
Manuel <manueljoseph113@gmail.com>
2021-01-09 19:43:25 -0500

    added user panel

--- src/components/library/indexPage/Main.tsx
+++ src/components/library/indexPage/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../../../styles/Library.module.css'
 import SideBar from './SideBar'
+import DisplayPanel from './DisplayPanel'
 
 export default function Main() {
 
@@ -9,8 +10,13 @@ export default function Main() {
             <aside className={styles['sidebar']}>
                 <SideBar />
             </aside>
-            <main>
-                main section
+            <main className={styles['user-panel-root']}>
+                <section>
+                    <div style={{height: 100}} />
+                </section>
+                <section>
+                    <DisplayPanel />
+                </section>
             </main>
         </div>
     )
