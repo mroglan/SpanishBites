f395dd46ae1c060c798cd2cf991a0b9c0f79dcfe
Manuel <manueljoseph113@gmail.com>
2021-01-15 20:48:12 -0500

    added static props

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
 import styles from '../../styles/Library.module.css'
+import { GetStaticProps } from 'next'
+import {getAllUnpopulatedAuthors} from '../../utils/authors'
+import {getAllUnpopulatedBooks} from '../../utils/books'
+import {getAllTimePeriods} from '../../utils/timePeriods'
+import {getAllGenres} from '../../utils/genres'
+import {getAllPassages} from '../../utils/passages'
 
 import MainHeader from '../../components/nav/MainHeader'
 import Main from '../../components/library/indexPage/Main'
 
-export default function Library() {
+export default function Library({items:dbItems}) {
 
     return (
         <>
@@ -27,4 +33,19 @@ export default function Library() {
             </div>
         </>
     )
+}
+
+export const getStaticProps:GetStaticProps = async () => {
+
+    const [authors, books, timePeriods, genres, passages] = await Promise.all([getAllUnpopulatedAuthors(), 
+        getAllUnpopulatedBooks(), getAllTimePeriods(), getAllGenres(), getAllPassages()])
+    
+    return {props: {items: {
+        authors: JSON.parse(JSON.stringify(authors)),
+        books: JSON.parse(JSON.stringify(books)),
+        timePeriods: JSON.parse(JSON.stringify(timePeriods)),
+        genres: JSON.parse(JSON.stringify(genres)),
+        passages: JSON.parse(JSON.stringify(passages))
+    }}}
+
 }
