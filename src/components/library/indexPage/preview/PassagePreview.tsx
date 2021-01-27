9b37c2104a5ae506f28f0be341456b53d354e65e
Manuel <manueljoseph113@gmail.com>
2021-01-26 20:35:08 -0500

    added passage preview

---
+++ src/components/library/indexPage/preview/PassagePreview.tsx
@@ -0,0 +1,45 @@
+import React, {useMemo} from 'react'
+import {ClientPassage, ClientUnpopulatedAuthor} from '../../../../database/dbInterfaces'
+import {Box, Typography} from '@material-ui/core'
+import styles from '../../../../styles/Library.module.css'
+import TextDisplay from '../../../mui-rte/TextDisplay'
+
+interface PreviewPassage extends Omit<ClientPassage, 'book'> {
+    book: {
+        _id: string;
+        title: string;
+        genres: string[];
+        timePeriod: string;
+        authors: ClientUnpopulatedAuthor[];
+    }
+}
+
+interface Props {
+    passage: PreviewPassage;
+}
+
+export default function PreviewPassage({passage}:Props) {
+
+    const authors = useMemo(() => passage.book.authors.map(({firstName, lastName}) => firstName + ' ' + lastName), [passage])
+
+    return (
+        <Box>
+            <Box textAlign="center">
+                <Typography variant="h4">
+                    {passage.name}
+                </Typography>
+            </Box>
+            <Box textAlign="center" mt={2}>
+                <Typography variant="h5">
+                    From <i>{passage.book.title}</i>
+                </Typography>
+                <Typography variant="h5">
+                    by {authors.join(', ')}
+                </Typography>
+            </Box>
+            <Box mx="auto" maxWidth="90ch" mt={3}>
+                <TextDisplay text={passage.spanishText} />
+            </Box>
+        </Box>
+    )
+}
