bcd4a39bfcb2f2f2a12aff0a22f225528f4fce55
Manuel <manueljoseph113@gmail.com>
2021-02-10 09:02:38 -0500

    added FiltersPanel tests

--- src/__tests__/unit/components/library/indexPage/FiltersPanel.test.tsx
+++ src/__tests__/unit/components/library/indexPage/FiltersPanel.test.tsx
@@ -11,28 +11,47 @@ const items:any = [
 
 const value = ['1', '2', '3']
 
-describe('BookSelect', () => {
+describe('FiltersPanel', () => {
 
-    const dispatch = jest.fn(val => null)
+    const filters = {potato: 'ggs'}
+    const setFilters = jest.fn(val => null)
+    const closeFilters = jest.fn(() => null)
 
-    const wrapper = mount(<BookSelect value={value} dispatch={dispatch} books={items} />)
+    const wrapper = shallow(<FiltersPanel filters={filters} setFilters={setFilters} closeFilters={closeFilters} />)
 
-    it('Displays correct options', () => {
-        console.log(wrapper.debug())
-        const autoComplete = wrapper.find('[data-testid="book-select-input"]').find('input')
+    it('Closes panels on cancel', () => {
+        wrapper.find('[data-testid="filterspanel-cancel"]').simulate('click')
+        expect(closeFilters).toHaveBeenCalled()
+    })
 
-        autoComplete.simulate('click')
-
-        const menuItem = wrapper.find('[data-testid="book-options"]').at(0)
-
-        menuItem.simulate('click')
-        
-        // autoComplete.simulate('change', {target: {value: '1'}})
-        // autoComplete.simulate('keydown', {keyCode: 40})
-        // autoComplete.simulate('keydown', {keyCode: 13})
-        
-        // input.simulate('change', {target: {value: '1'}})
-        // expect(dispatch).toHaveBeenCalledWith({type: 'CHANGE_BOOKS', payload: ['1']})
-        expect(5).toEqual(5)
+    it('Saves filters on save', () => {
+        wrapper.find('[data-testid="filterspanel-save"]').simulate('click')
+        expect(setFilters).toHaveBeenCalledWith({potato: 'ggs'})
     })
 })
+
+// describe('BookSelect', () => {
+
+//     // const dispatch = jest.fn(val => null)
+
+//     // const wrapper = mount(<BookSelect value={value} dispatch={dispatch} books={items} />)
+
+//     it('Displays correct options', () => {
+//         // console.log(wrapper.debug())
+//         // const autoComplete = wrapper.find('[data-testid="book-select-input"]').find('input')
+
+//         // autoComplete.simulate('click')
+
+//         // const menuItem = wrapper.find('[data-testid="book-options"]').at(0)
+
+//         // menuItem.simulate('click')
+        
+//         // autoComplete.simulate('change', {target: {value: '1'}})
+//         // autoComplete.simulate('keydown', {keyCode: 40})
+//         // autoComplete.simulate('keydown', {keyCode: 13})
+        
+//         // input.simulate('change', {target: {value: '1'}})
+//         // expect(dispatch).toHaveBeenCalledWith({type: 'CHANGE_BOOKS', payload: ['1']})
+//         expect(5).toEqual(5)
+//     })
+// })
