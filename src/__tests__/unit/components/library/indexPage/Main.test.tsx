c458835e2f9c27833a89cbc9c20ccf6bda10ec0d
Manuel <manueljoseph113@gmail.com>
2021-02-10 08:45:16 -0500

    added tests

---
+++ src/__tests__/unit/components/library/indexPage/Main.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import Main from '../../../../../components/library/indexPage/Main'
+
+const libraryItems = {authors: [], books: [], timePeriods: [], genres: [], passages: [], bite: {
+    _id: '', author: '', name: '', image: '', work: '', text: '', desc: '', dates: []
+}}
+
+describe('Main Library', () => {
+
+    const wrapper = shallow(<Main items={libraryItems} />)
+
+    it('Displays the sidebar', () => {
+        expect(wrapper.find('[data-testid="library-sidebar"]').length).toEqual(1)
+    })
+
+    it('Displays the search panel', () => {
+        expect(wrapper.find('[data-testid="searchpanel-section"]').length).toEqual(1)
+    })
+
+    it('Displays the active filters', () => {
+        expect(wrapper.find('[data-testid="filtersdisplay-section"]').length).toEqual(1)
+    })
+
+    it('Displays the display panel', () => {
+        expect(wrapper.find('[data-testid="displaypanel-section"]').length).toEqual(1)
+    })
+})
