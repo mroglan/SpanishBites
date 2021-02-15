5640ca98785863817894e03fe4c83a58e110a569
Manuel <manueljoseph113@gmail.com>
2021-02-15 17:58:52 -0500

    added PremiumDisplay

---
+++ src/components/library/passages/PremiumDisplay.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../../../styles/Resource.module.css'
+import {Paper, Box, Typography} from '@material-ui/core'
+
+export default function PremiumDisplay() {
+
+    return (
+        <div className={styles['no-premium-message-root']}>
+            <Paper elevation={3}>
+                <Box p={3} textAlign="center">
+                    <Typography variant="body1">
+                        Analysis, audio, and annotations coming soon with Spanish Bites Premium!
+                    </Typography>
+                </Box>
+            </Paper>
+        </div>
+    )
+}
