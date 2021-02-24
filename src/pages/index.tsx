41e8f53a2bcfa1d9f2bb2a8029eb52c1961f468e
Manuel <manueljoseph113@gmail.com>
2021-02-24 13:44:09 -0500

    linked to Library component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import MainHeader from '../components/nav/MainHeader'
 import MainSideBar from '../components/nav/MainSideNav'
 import MainFooter from '../components/nav/MainFooter'
 import FirstBanner from '../components/home/FirstBanner'
+import Library from '../components/home/Library'
 
 export default function Home() {
 
@@ -21,11 +22,11 @@ export default function Home() {
                 <div className={styles['sub-ad']}>
                     <FirstBanner />
                 </div>
-                <div className={styles['side-bar']}>
+                {/* <div className={styles['side-bar']}>
                     <MainSideBar />
-                </div>
+                </div> */}
                 <div className={styles.main}>
-                    Main Section for Bites and Such
+                    <Library />
                 </div>
                 <div className={styles.footer}>
                     <MainFooter />
