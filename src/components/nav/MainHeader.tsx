938cbf07f6fc6716b80fea036c7bc02a3ea88d7d
Manuel <manueljoseph113@gmail.com>
2021-02-25 17:49:11 -0500

    added library link

--- src/components/nav/MainHeader.tsx
+++ src/components/nav/MainHeader.tsx
@@ -1,41 +1,47 @@
 import React from 'react'
 import {Box, Typography, Grid} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
+import Link from 'next/link'
+import styles from '../../styles/Header.module.css'
 
 interface Props {
     bg: string;
 }
 
-const useStyles = makeStyles(theme => ({
-    title: {
-        color: theme.palette.primary.main,
-        fontWeight: 600,
-        whiteSpace: 'nowrap',
-        fontFamily: 'Catamaran !important'
-    },
-}))
-
 export default function Header({bg}:Props) {
 
-    const classes = useStyles()
     return (
         <Box>
-            <Box py={1} px={2} textAlign="center" style={{background: bg}}>
+            <Box  className={styles['main-root']} textAlign="center" style={{background: bg}}>
                 <Grid container alignItems="center" justify="space-between">
+                    <Grid item>
+                        <Link href="/">
+                            <a className={styles.link}>
+                                <Grid container wrap="nowrap" spacing={1} alignItems="center">
+                                    <Grid item>
+                                        <img src="/logo3.svg" title="Spanish Bites" style={{width: 40, height: 40}} />
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography className={styles.title} variant="h4" component="h1">
+                                            Spanish Bites
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </a>
+                        </Link>
+                    </Grid>
                     <Grid item>
                         <Grid container wrap="nowrap" spacing={1} alignItems="center">
                             <Grid item>
-                                <img src="/logo3.svg" title="Spanish Bites" style={{width: 40, height: 40}} />
-                            </Grid>
-                            <Grid item>
-                                <Typography className={classes.title} variant="h4" component="h1">
-                                    Spanish Bites
-                                </Typography>
+                                <Link href="/library">
+                                    <a>
+                                        <Typography className={styles.link} variant="h6">
+                                            Library
+                                        </Typography>
+                                    </a>
+                                </Link>
                             </Grid>
                         </Grid>
-                    </Grid>
-                    <Grid item>
-
                     </Grid>
                 </Grid>
             </Box>
