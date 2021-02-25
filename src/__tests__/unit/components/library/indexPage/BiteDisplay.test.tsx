8b9ebc68b59dc1a9035b52365e7716cc0f12aa9d
Manuel <manueljoseph113@gmail.com>
2021-02-25 17:54:55 -0500

    removed some tests moved to another component

--- src/__tests__/unit/components/library/indexPage/BiteDisplay.test.tsx
+++ src/__tests__/unit/components/library/indexPage/BiteDisplay.test.tsx
@@ -14,15 +14,15 @@ describe('BiteDisplay', () => {
         expect(hideBite).toHaveBeenCalled()
     })
 
-    it('Includes author name and work', () => {
-        expect(wrapper.find('[data-testid="bite-citation"]').length).toEqual(1)
-    })
+    // it('Includes author name and work', () => {
+    //     expect(wrapper.find('[data-testid="bite-citation"]').length).toEqual(1)
+    // })
 
-    it('Includes the text', () => {
-        expect(wrapper.find('[data-testid="bite-text"]').length).toEqual(1)
-    })
+    // it('Includes the text', () => {
+    //     expect(wrapper.find('[data-testid="bite-text"]').length).toEqual(1)
+    // })
 
-    it('Includes desc', () => {
-        expect(wrapper.find('[data-testid="bite-desc"]').length).toEqual(1)
-    })
+    // it('Includes desc', () => {
+    //     expect(wrapper.find('[data-testid="bite-desc"]').length).toEqual(1)
+    // })
 })
