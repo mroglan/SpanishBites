50829d3b29c2e995c8f28854683afab38ddef0ca
Manuel <manueljoseph113@gmail.com>
2021-03-03 17:21:27 -0500

    updated to fauna queries

--- src/utils/books.ts
+++ src/utils/books.ts
@@ -1,55 +1,70 @@
-import database from '../database/database'
 import {DBBook} from '../database/dbInterfaces'
-import {ObjectId} from 'mongodb'
+import {client} from '../database/fauna-db'
+import {query as q} from 'faunadb'
 
 export const getAllUnpopulatedBooks = async () => {
-    const db = await database()
 
-    const books:DBBook[] = await db.collection('books').find({}).toArray()
+    const books:any = await client.query(
+        q.Map(q.Paginate(q.Match(q.Index('all_books'))), (ref) => q.Get(ref))
+    )
 
-    return books
+    return books.data.map(d => ({...d.data, _id: d.ref.id, timePeriod: d.timePeriod?.id || '', genres: d.genres.map(g => g?.id || ''),
+        authors: d.authors.map(a => a?.id || '')
+    }))
 }
 
 export const getAllBooks = async () => {
-    const db = await database()
 
-    const books:DBBook[] = await db.collection('books').aggregate([
-        {$lookup: {
-            from: 'authors',
-            localField: 'authors',
-            foreignField: '_id',
-            as: 'authors'
-        }}
-    ]).toArray()
+    const books:any = await client.query(
+        q.Map(q.Paginate(q.Match(q.Index('all_books')), {size: 1000}), 
+            q.Lambda('bookRef', q.Let(
+                {bookDoc: q.Get(q.Var('bookRef'))}, 
+                q.Merge(
+                    q.Select(['data'], q.Var('bookDoc')), 
+                    [
+                        {_id: q.Select(['ref', 'id'], q.Var('bookDoc'))},
+                        {authors: q.Map(q.Select(['data', 'authors'], q.Var('bookDoc')), q.Lambda('authorRef', q.If(
+                            q.Exists(q.Ref(q.Collection('authors'), q.Select(['id'], q.Var('authorRef')))),
+                            q.Merge(q.Select(['data'], q.Get(q.Var('authorRef'))), {_id: q.Select(['id'], q.Var('authorRef'))}),
+                            null
+                        )))}
+                    ]
+                )
+            ))
+        )
+    )
 
-    return books
+    return books.data.map(d => ({...d, timePeriod: d.timePeriod?.id || '', genres: d.genres.map(g => g?.id || '')}))
 }
 
-export const getBook = async (id:ObjectId) => {
-    const db = await database()
+export const getBook = async (id:string) => {
 
-    const book:DBBook[] = await db.collection('books').aggregate([
-        {$match: {'_id': new ObjectId(id)}},
-        {$lookup: {
-            from: 'timePeriods',
-            localField: 'timePeriod',
-            foreignField: '_id',
-            as: 'timePeriod'
-        }},
-        {$unwind: '$timePeriod'},
-        {$lookup: {
-            from: 'authors',
-            localField: 'authors',
-            foreignField: '_id',
-            as: 'authors'
-        }},
-        {$lookup: {
-            from: 'genres',
-            localField: 'genres',
-            foreignField: '_id',
-            as: 'genres'
-        }}
-    ]).toArray()
+    const book:any = await client.query(
+        q.Let(
+            {bookDoc: q.Get(q.Ref(q.Collection('books'), id))},
+            q.Merge(
+                q.Select(['data'], q.Var('bookDoc')), 
+                [
+                    {_id: q.Select(['ref', 'id'], q.Var('bookDoc'))},
+                    {authors: q.Map(q.Select(['data', 'authors'], q.Var('bookDoc')), q.Lambda('authorRef', q.If(
+                        q.Exists(q.Ref(q.Collection('authors'), q.Select(['id'], q.Var('authorRef')))),
+                        q.Merge(q.Select(['data'], q.Get(q.Var('authorRef'))), {_id: q.Select(['id'], q.Var('authorRef'))}),
+                        null
+                    )))},
+                    {genres: q.Map(q.Select(['data', 'genres'], q.Var('bookDoc')), q.Lambda('genreRef', q.If(
+                        q.Exists(q.Ref(q.Collection('genres'), q.Select(['id'], q.Var('genreRef')))),
+                        q.Merge(q.Select(['data'], q.Get(q.Var('genreRef'))), {_id: q.Select(['id'], q.Var('genreRef'))}),
+                        null
+                    )))},
+                    {timePeriod: q.If(
+                        q.Exists(q.Ref(q.Collection('timePeriods'), q.Select(['data', 'timePeriod', 'id'], q.Var('bookDoc')))),
+                        q.Merge(q.Select(['data'], q.Get(q.Select(['data', 'timePeriod'], q.Var('bookDoc')))), {_id: q.Select(['data', 'timePeriod', 'id'], q.Var('bookDoc'))}),
+                        null
+                    )}
+                ]
+            )
+        )
+    )
 
-    return book[0]
+    return book
 }
