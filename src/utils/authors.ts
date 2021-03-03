11de9ef1a2ea769c4d555e93e0d699a2154f8377
Manuel <manueljoseph113@gmail.com>
2021-03-03 15:43:33 -0500

    updated to fauna queries

--- src/utils/authors.ts
+++ src/utils/authors.ts
@@ -1,48 +1,55 @@
-import database from '../database/database'
 import {DBAuthor} from '../database/dbInterfaces'
-import {ObjectId} from 'mongodb'
+import { client } from '../database/fauna-db'
+import {query as q} from 'faunadb'
+
+const populateAuthorDoc = (doc) => {
+    return q.Merge(
+        q.Select(['data'], doc),
+        [
+            {timePeriod: q.If(
+                q.Exists(q.Ref(q.Collection('timePeriods'), q.Select(['data', 'timePeriod', 'id'], doc))),
+                q.Merge(q.Select(['data'], q.Get(q.Select(['data', 'timePeriod'], doc))), {_id: q.Select(['data', 'timePeriod', 'id'], doc)}),
+                null
+            )},
+            {_id: q.Select(['ref', 'id'], doc)}
+        ]
+    )
+}
 
 export const getAllAuthors = async () => {
-    const db = await database()
 
-    const authors:DBAuthor[] = await db.collection('authors').aggregate([
-        {$lookup: {
-            from: 'timePeriods',
-            localField: 'timePeriod',
-            foreignField: '_id',
-            as: 'timePeriod'
-        }},
-        {$unwind: '$timePeriod'},
-        {$unset: 'detailedInfo'}
-    ]).sort({'lastName': 1}).toArray()
+    const authors:any = await client.query(
+        q.Map(q.Paginate(q.Match(q.Index('all_authors')), {size: 1000}),
+            q.Lambda('authorRef', q.Let(
+                {authorDoc: q.Get(q.Var('authorRef'))},
+                populateAuthorDoc(q.Var('authorDoc'))
+            ))
+        )
+    )
     
-    return authors
+    return authors.data
 }
 
 export const getAllUnpopulatedAuthors = async () => {
-    const db = await database()
 
-    const authors:DBAuthor[] = await db.collection('authors').aggregate([
-        {$unset: 'detailedInfo'}
-    ]).sort({'lastName': 1}).toArray()
+    const authors:any = await client.query(
+        q.Map(q.Paginate(q.Match(q.Index('all_authors')), {size: 1000}), (ref) => q.Get(ref))
+    )
 
-    return authors
+    return authors.data.map(d => {
+        delete d.data.detailedInfo
+        return ({...d.data, _id: d.ref.id, timePeriod: d.data.timePeriod?.id || ''})
+    })
 }
 
-export const getAuthor = async (id:ObjectId) => {
-    const db = await database()
+export const getAuthor = async (id:string) => {
 
-    const author:DBAuthor[] = await db.collection('authors').aggregate([
-        {$match: {'_id': id}},
-        {$lookup: {
-            from: 'timePeriods',
-            localField: 'timePeriod',
-            foreignField: '_id',
-            as: 'timePeriod'
-        }},
-        {$unwind: '$timePeriod'},
-        {$unset: 'detailedInfo'}
-    ]).toArray()
+    const author:any = await client.query(
+        q.Let(
+            {authorDoc: q.Get(q.Ref(q.Collection('authors'), id))},
+            populateAuthorDoc(q.Var('authorDoc'))
+        )
+    )
 
-    return author[0]
+    return author
 }
