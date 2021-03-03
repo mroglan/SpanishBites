b0dd1c9d9f9fa64777bf3a486e02e6e5d781bcd6
Manuel <manueljoseph113@gmail.com>
2021-03-03 13:53:44 -0500

    updated to fauna queries

--- src/utils/timePeriods.ts
+++ src/utils/timePeriods.ts
@@ -1,17 +1,23 @@
-import database from '../database/database'
 import {DBTimePeriod} from '../database/dbInterfaces'
-import {ObjectId} from 'mongodb'
+import {client} from '../database/fauna-db'
+import {query as q} from 'faunadb'
 
 export const getAllTimePeriods = async () => {
-    const db = await database()
-    const timePeriods:DBTimePeriod[] = await db.collection('timePeriods').find({}).toArray()
+
+    const rawPeriods:any = await client.query(
+        q.Map(q.Paginate(q.Match(q.Index('all_timePeriods'))), (ref) => q.Get(ref))
+    )
+
+    const timePeriods = rawPeriods.data.map(period => ({...period.data, _id: period.ref.id}))
 
     return timePeriods.sort((a, b) => Number(a.dateRange[0]) - Number(b.dateRange[0]))
 }
 
-export const getTimePeriod = async (id:ObjectId) => {
-    const db = await database()
-    const timePeriod:DBTimePeriod = await db.collection('timePeriods').findOne({'_id': id})
+export const getTimePeriod = async (id:string) => {
+
+    const timePeriod:any = await client.query(
+        q.Get(q.Ref(q.Collection('timePeriods'), id))
+    )
     
-    return timePeriod
+    return {...timePeriod.data, _id: timePeriod.ref.id}
 }
