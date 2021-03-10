87ee41991f7a9f2157f2f1a50da1bae26eb96d5d
Manuel <manueljoseph113@gmail.com>
2021-03-10 17:48:19 -0500

    added TextField

---
+++ src/components/forms/FormikFields.tsx
@@ -0,0 +1,13 @@
+import {useField, Field} from 'formik'
+import {TextField} from '@material-ui/core'
+
+export const FormikTextField = (props) => {
+    const [field, meta] = useField({
+        name: props.name,
+        type: props.type || 'text',
+    })
+    return (
+        <Field {...props} {...field} as={TextField} error={meta.touched && meta.error ? true : false} 
+        helperText={meta.touched && meta.error ? meta.error : ''} />
+    )
+}
