75f30c53df48e1d32c1a6871ddd3b42c08f81cc5
Manuel <manueljoseph113@gmail.com>
2021-03-11 09:00:06 -0500

    added skeleton

---
+++ src/pages/signup.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Head from 'next/head'
+import SimpleHeader from '../components/nav/SimpleHeader'
+import Main from '../components/signup/Main'
+import MainFooter from '../components/nav/MainFooter'
+import styles from '../styles/Basic.module.css'
+
+export default function SignUp() {
+
+    return (
+        <>
+            <Head>
+                <title>Sign up for Spanish Bites</title>
+            </Head>
+            <div className={styles.root}>
+                <div className={styles.header}>
+                    <SimpleHeader bg="none" />
+                </div>
+                <div className={styles.main}>
+                    <Main />
+                </div>
+                <div className={styles.footer}>
+                    <MainFooter />
+                </div>
+            </div>
+        </>
+    )
+}
