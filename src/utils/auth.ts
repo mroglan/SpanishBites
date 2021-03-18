ea824ff771c6b5fcae365df3874fb1b77ca097df
Manuel <manueljoseph113@gmail.com>
2021-03-18 19:52:05 -0400

    added ensureAuth

---
+++ src/utils/auth.ts
@@ -0,0 +1,31 @@
+import {GetServerSidePropsContext, NextApiHandler, NextApiRequest, NextApiResponse} from 'next'
+import jwt from 'jsonwebtoken'
+import Router from 'next/router'
+import {parseCookies} from 'nookies'
+
+const redirectTo = (ctx:GetServerSidePropsContext, url:string) => {
+    if(!ctx.req) {
+        Router.replace('url')
+    } else {
+        ctx.res.writeHead(302, {
+            Location: `${process.env.BASE_URL}${url}`
+        })
+        ctx.res?.end()
+    }
+}
+
+export async function ensureAuth(ctx:GetServerSidePropsContext) {
+    try {
+        const {auth} = parseCookies(ctx)
+
+        await new Promise((res, rej) => {
+            jwt.verify(auth, process.env.SIGNATURE, (err, decoded) => {
+                if(!err && decoded) res(decoded)
+                rej('Not Authenticated')
+            })
+        })
+    } catch(e) {
+        redirectTo(ctx, '/login')
+        return {}
+    }
+}
