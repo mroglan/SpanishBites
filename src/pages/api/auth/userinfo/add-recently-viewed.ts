46b22c8c34639548fe54a1eff4a56909a1e6d376
Manuel <manueljoseph113@gmail.com>
2021-03-19 18:01:26 -0400

    added api

---
+++ src/pages/api/auth/userinfo/add-recently-viewed.ts
@@ -0,0 +1,31 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import {getUserFromApi, addToRecentlyAdded, getUser} from '../../../../utils/users'
+
+export default async function UpdateRecentlyAdded(req:NextApiRequest, res:NextApiResponse) {
+
+    if(req.method !== 'POST') {
+        return res.status(403).json({msg: 'ok...'})
+    }
+
+    try {
+
+        const clientUser:any =  await getUserFromApi(req)
+
+        if(!clientUser) return res.status(403).json({msg: 'Not Signed In'})
+
+        const user = await getUser(clientUser._id)
+
+        if(!user.recentlyViewed) {
+            user.recentlyViewed  = [req.body.item]
+        } else {
+            user.recentlyViewed.filter(item => item.id !== req.body.item.id)
+            user.recentlyViewed.unshift(req.body.item)
+        }
+
+        await addToRecentlyAdded(user._id, user.recentlyViewed)
+        
+        return res.status(200).json({msg: 'Updated Recently Viewed'})
+    } catch(e) {
+        return res.status(500).json({msg: 'Internal server error'})
+    }
+}
