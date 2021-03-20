c71d33e01292c1122bbba5ba4557f3309fcec520
Manuel <manueljoseph113@gmail.com>
2021-03-20 09:49:04 -0400

    added skeleton

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,29 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import React from 'react'
 import { ensureAuth } from '../../utils/auth'
+import styles from '../../styles/Basic.module.css'
+import MainHeader from '../../components/nav/MainHeader'
+import MainFooter from '../../components/nav/MainFooter'
+import Main from '../../components/dashboard/Main'
 
-export default function Dashboard() {
+export default function Dashboard({user}) {
 
     return (
-        <div>
-            dashboard
+        <div className={styles.root}>
+            <div className={styles.header}>
+                <MainHeader bg="none" user={user} />
+            </div>
+            <div className={styles.main}>
+                <Main />
+            </div>
+            <div className={styles.footer}>
+                <MainFooter />
+            </div>
         </div>
     )
 }
 
 export const getServerSideProps:GetServerSideProps = async (ctx:GetServerSidePropsContext) => {
-    await ensureAuth(ctx)
-    return {props: {}}
+    const user = await ensureAuth(ctx)
+    return {props: {user: JSON.parse(JSON.stringify(user))}}
 }
