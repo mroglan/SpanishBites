6db5c830a2d39c47128d5cfe4041154943fbed34
Manuel <manueljoseph113@gmail.com>
2021-03-20 08:14:33 -0400

    returned user or {}

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -18,12 +18,14 @@ export async function ensureAuth(ctx:GetServerSidePropsContext) {
     try {
         const {auth} = parseCookies(ctx)
 
-        await new Promise((res, rej) => {
+        const user = await new Promise((res, rej) => {
             jwt.verify(auth, process.env.SIGNATURE, (err, decoded) => {
                 if(!err && decoded) res(decoded)
                 rej('Not Authenticated')
             })
         })
+
+        return user
     } catch(e) {
         redirectTo(ctx, '/login')
         return {}
