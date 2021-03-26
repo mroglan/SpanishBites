208c3da1d77e09905042b58e2d01647f3a8720f6
Manuel <manueljoseph113@gmail.com>
2021-03-26 13:39:13 -0400

    added BiteSize section

---
+++ src/components/about/BiteSize.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from '../../styles/About.module.css'
+import {Box, Typography} from '@material-ui/core'
+import Image from 'next/image'
+
+export default function BiteSize() {
+
+    return (
+        <Box my={6} mx={3}>
+            <div className={styles['c1-container']}>
+                <div className={styles['c1-graphic-container']}>
+                    <img src="/about/magnifyText.png" />
+                </div>
+                <div className={styles['c1-text-container']}>
+                    <div>
+                        <Typography variant="h3" gutterBottom className={`${styles['catamaran']} ${styles['title-text']}`}>
+                            Making Literature Bite Size
+                        </Typography>
+                        <Typography variant="body1" className={`${styles['catamaran']} ${styles['body-text']}`}>
+                            Every day, you can visit the Spanish Bites library to see a new “bite” of Spanish literature, 
+                            which briefly explain a figurative or syntactical device used in literature with a short excerpt. 
+                        </Typography>
+                    </div>
+                </div>
+            </div>
+        </Box>
+    )
+}
