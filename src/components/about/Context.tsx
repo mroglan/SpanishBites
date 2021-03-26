6b7db03b292f4c2d8b395ca651ffa901a8400f34
Manuel <manueljoseph113@gmail.com>
2021-03-26 17:50:25 -0400

    added Context

---
+++ src/components/about/Context.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from '../../styles/About.module.css'
+import {Box, Typography} from '@material-ui/core'
+
+export default function Context() {
+
+    return (
+        <Box position="relative" py={6} px={6}>
+            <div className={styles['c1-container']}>
+                <div className={styles['c1-graphic-container']}>
+                    <img src="/about/context.png" />
+                </div>
+                <div className={styles['c1-text-container']}>
+                    <div>
+                        <Typography variant="h3" gutterBottom className={`${styles['catamaran']} ${styles['title-text']}`}>
+                            Knowing What's Going On
+                        </Typography>
+                        <Typography variant="body1" className={`${styles['catamaran']} ${styles['body-text']}`}>
+                            Crucial to understanding any piece in literature is knowing its context- what events, movements, or ideas were shaping society when it was written. 
+                            Without this, it’s hard to appreciate a literary work. With timelines, descriptions, and analysis encompassing many works, 
+                            Spanish Bites provides you with a foundation to start reading.
+                        </Typography>
+                    </div>
+                </div>
+            </div>
+            <div className={`${styles['white-triangle']} ${styles['triangle']}`} />
+        </Box>
+    )
+}
