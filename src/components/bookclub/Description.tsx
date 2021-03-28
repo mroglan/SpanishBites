754dc47589e282058c07a99e286da76124b2279e
Manuel <manueljoseph113@gmail.com>
2021-03-28 15:51:09 -0400

    fixed typo

--- src/components/bookclub/Description.tsx
+++ src/components/bookclub/Description.tsx
@@ -8,7 +8,7 @@ export default function Description() {
         <Box my={3} maxWidth={900} textAlign="center" mx="auto" px={3}>
             <Typography variant="body1" className={`${styles.catamaran} ${styles['desc-text']}`}>
                 We are excited to start the book club soon. If you are interested in joining the book club, you will need to create
-                an account to sign up a club meeting. We are considering many different books and meeting times for our first month,
+                an account to sign up for a club meeting. We are considering many different books and meeting times for our first month,
                 so feel free to complete (only once) the form below to give us your input!
             </Typography>
         </Box>
