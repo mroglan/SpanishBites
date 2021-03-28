cda19c567207814c4aa87aa2e3447c12d586b174
Manuel <manueljoseph113@gmail.com>
2021-03-28 10:11:59 -0400

    changed header when no bg

--- src/components/nav/MainHeader.tsx
+++ src/components/nav/MainHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ClientCookieUser} from '../../database/dbInterfaces'
-import {Box, Typography, Grid, Avatar} from '@material-ui/core'
+import {Box, Typography, Grid, Avatar, Divider, Paper} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import Link from 'next/link'
 import styles from '../../styles/Header.module.css'
@@ -12,74 +12,89 @@ interface Props {
     user?: ClientCookieUser;
 }
 
+function HeaderContent({user, bg}) {
+
+    return (
+        <Box py={2} className={styles['main-root']} textAlign="center" style={{background: bg}}>
+            <Grid container alignItems="center" justify="space-between">
+                <Grid item>
+                    <Link href="/">
+                        <a className={styles.link}>
+                            <Grid container wrap="nowrap" spacing={1} alignItems="center">
+                                <Grid item>
+                                    <img src="/logo3.svg" title="Spanish Bites" style={{width: 40, height: 40}} />
+                                </Grid>
+                                <Grid item>
+                                    <Typography className={styles.title} variant="h4" component="h1">
+                                        Spanish Bites
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </a>
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Grid container wrap="nowrap" spacing={3} alignItems="center">
+                        <Grid item>
+                            <Link href="/about">
+                                <a>
+                                    <Typography className={styles.link} variant="h6">
+                                        About
+                                    </Typography>
+                                </a>
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/bookclub">
+                                <a>
+                                    <Typography className={styles.link} variant="h6">
+                                        Book Club
+                                    </Typography>
+                                </a>
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/library">
+                                <a>
+                                    <Typography className={styles.link} variant="h6">
+                                        Library
+                                    </Typography>
+                                </a>
+                            </Link>
+                        </Grid>
+                        {user ? <Grid item>
+                            <UserNavDropdown user={user} />
+                        </Grid> : <Grid style={{marginLeft: '1rem'}} item>
+                            <Link href="/login">
+                                <a>
+                                    <BlueDenseButton>
+                                        <Typography variant="body1">
+                                            Sign in
+                                        </Typography>
+                                    </BlueDenseButton>
+                                </a>
+                            </Link>
+                        </Grid>}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
+
 export default function Header({bg, user}:Props) {
+
+    if(bg === 'none') {
+        return (
+            <Paper style={{background: bg}} square elevation={3}>
+                <HeaderContent bg={bg} user={user} />
+            </Paper>
+        )
+    }
 
     return (
         <Box>
-            <Box py={2} className={styles['main-root']} textAlign="center" style={{background: bg}}>
-                <Grid container alignItems="center" justify="space-between">
-                    <Grid item>
-                        <Link href="/">
-                            <a className={styles.link}>
-                                <Grid container wrap="nowrap" spacing={1} alignItems="center">
-                                    <Grid item>
-                                        <img src="/logo3.svg" title="Spanish Bites" style={{width: 40, height: 40}} />
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography className={styles.title} variant="h4" component="h1">
-                                            Spanish Bites
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-                            </a>
-                        </Link>
-                    </Grid>
-                    <Grid item>
-                        <Grid container wrap="nowrap" spacing={3} alignItems="center">
-                            <Grid item>
-                                <Link href="/about">
-                                    <a>
-                                        <Typography className={styles.link} variant="h6">
-                                            About
-                                        </Typography>
-                                    </a>
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="/bookclub">
-                                    <a>
-                                        <Typography className={styles.link} variant="h6">
-                                            Book Club
-                                        </Typography>
-                                    </a>
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="/library">
-                                    <a>
-                                        <Typography className={styles.link} variant="h6">
-                                            Library
-                                        </Typography>
-                                    </a>
-                                </Link>
-                            </Grid>
-                            {user ? <Grid item>
-                                <UserNavDropdown user={user} />
-                            </Grid> : <Grid style={{marginLeft: '1rem'}} item>
-                                <Link href="/login">
-                                    <a>
-                                        <BlueDenseButton>
-                                            <Typography variant="body1">
-                                                Sign in
-                                            </Typography>
-                                        </BlueDenseButton>
-                                    </a>
-                                </Link>
-                            </Grid>}
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Box>
+            <HeaderContent bg={bg} user={user} />
         </Box>
     )
 }
