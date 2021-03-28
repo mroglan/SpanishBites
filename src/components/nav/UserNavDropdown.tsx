a4a564925f800e61cb111df800f6e208db786961
Manuel <manueljoseph113@gmail.com>
2021-03-28 10:51:53 -0400

    fixed Popper zIndex

--- src/components/nav/UserNavDropdown.tsx
+++ src/components/nav/UserNavDropdown.tsx
@@ -49,7 +49,7 @@ export default function UserNavDropdown({user}:Props) {
     return (
         <div className={classes.root}>
             <Avatar alt={user.name} className={classes.avatar} src={user.image || '/no-profile.jpg'} onClick={(e) => handleAvatarClick(e)} />
-            <Popper open={Boolean(anchorEl)} anchorEl={anchorEl} role="nav" transition placement="bottom-end">
+            <Popper style={{zIndex: 2500}} open={Boolean(anchorEl)} anchorEl={anchorEl} role="nav" transition placement="bottom-end">
                 <Paper elevation={3} >
                     <ClickAwayListener onClickAway={handleClose}>
                         <div>
