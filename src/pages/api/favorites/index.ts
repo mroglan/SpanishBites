6372ca45425ef861094472e2f0089049a726dd66
Manuel <manueljoseph113@gmail.com>
2021-05-19 11:43:27 -0400

    added update-favorites

--- src/pages/api/favorites/index.ts
+++ src/pages/api/favorites/index.ts
@@ -1,22 +1,29 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {verifyUser} from '../../../utils/auth'
-import { getFavorites } from '../../../utils/favorites'
+import { getFavorites, updateFavorites } from '../../../utils/favorites'
+import {getUserFromApi} from '../../../utils/users'
 
-export default verifyUser(async function Favorites(req:NextApiRequest, res:NextApiResponse) {
-
-    if(req.method !== 'POST') {
-        return res.status(400).json({msg: 'ok...'})
-    }
+export default async function Favorites(req:NextApiRequest, res:NextApiResponse) {
 
     try {
 
-        const {operation, jwtUser} = req.body
+        const jwtUser = await getUserFromApi(req)
 
-        if(operation === 'get-favorites') {
+        if(!jwtUser) return res.status(401).json({msg: 'YOU CANNOT PASS'})
+
+        const {operation, favorites} = req.body
+
+        if(req.method === 'GET') {
             const favorites = await getFavorites(jwtUser._id)
             return res.status(200).json(favorites)
         }
+        if(operation === 'update-favorites') {
+            await updateFavorites(jwtUser._id, favorites)
+            return res.status(200).json({msg: 'successfull update'})
+        }
+
+        return res.status(422).json({msg: 'Invalid request'})
     } catch(e) {
         return res.status(500).json({msg: 'Internal server error'})
     }
-})
+}
