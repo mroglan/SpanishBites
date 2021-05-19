4ffe62e3c03c64b0d1ba9d57f448dbe0388e08c6
Manuel <manueljoseph113@gmail.com>
2021-05-19 11:10:25 -0400

    added updateFavorites

--- src/utils/favorites.ts
+++ src/utils/favorites.ts
@@ -1,6 +1,7 @@
-import {DBUser} from '../database/dbInterfaces'
+import {DBUser, GeneralItem} from '../database/dbInterfaces'
 import {client} from '../database/fauna-db'
 import {query as q} from 'faunadb'
+import {getUser} from './users'
 
 export const getFavorites = async (id:string) => {
 
@@ -11,4 +12,11 @@ export const getFavorites = async (id:string) => {
     if(!user || !user.data.favorites) return []
 
     return user.data.favorites
+}
+
+export const updateFavorites = async (id:string, favorites:GeneralItem[]) => {
+
+    await client.query(
+        q.Update(q.Ref(q.Collection('users'), id), {data: {favorites}})
+    )
 }
