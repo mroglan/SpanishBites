9ef083a2cff5e516d36eda459c2feb158dab7af3
Manuel <manueljoseph113@gmail.com>
2021-05-30 13:51:31 -0400

    added premium api for authors

---
+++ src/pages/api/authors/[id]/premium.ts
@@ -0,0 +1,17 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import {getPremiumInfo} from '../../../../utils/authors'
+import {verifyPremiumUser} from '../../../../utils/auth'
+
+export default verifyPremiumUser(async function Premium(req:NextApiRequest, res:NextApiResponse) {
+
+    const id = req.query.id as string
+
+    try {
+
+        const info = await getPremiumInfo(id)
+
+        return res.status(200).json(info)
+    } catch(e) {
+        return res.status(500).json({msg: 'Internal server error'})
+    }
+})
