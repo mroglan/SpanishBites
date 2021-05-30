696c352bdacad9583e53287983eb96f8a4d3bb46
Manuel <manueljoseph113@gmail.com>
2021-05-30 13:51:07 -0400

    added premium section

--- src/components/library/authors/indiv/Main.tsx
+++ src/components/library/authors/indiv/Main.tsx
@@ -10,6 +10,7 @@ import useSWR, {mutate} from 'swr'
 import StarIcon from '@material-ui/icons/Star';
 import axios from 'axios'
 import {changeFavorites} from '../../../../utils/library'
+import TextDisplay from '../../../mui-rte/TextDisplay'
 
 interface Props {
     author: ClientAuthor;
@@ -18,6 +19,8 @@ interface Props {
 export default function Main({author}:Props) {
 
     const {data: favorites, isValidating}:{data?:GeneralItem[];isValidating:boolean} = useSWR('/api/favorites', {shouldRetryOnError: false})
+
+    const {data: premiumInfo}:{data?:{detailedInfo:string}} = useSWR(`/api/authors/${author._id}/premium`, {shouldRetryOnError: false})
 
     const [loading, setLoading] = useState(false)
 
@@ -33,103 +36,123 @@ export default function Main({author}:Props) {
         return !!favorites.find(fav => fav.id === author._id)
     }, [isValidating, favorites])
 
+    console.log(premiumInfo)
+
     return (
         <>
-            <div className={styles['img-container']}>
-                <img src={author.image || '/no-profile.jpg'} title={author.firstName + ' ' + author.lastName} />
-                {favorites && isFavorite && <div className={styles.star}> 
-                    <StarIcon fontSize="large" />
-                </div>}
-            </div>
-            <div className={styles['content-container']}>
-                <Paper elevation={3}>
-                    <Box px={3}>
-                        <Box>
-                            <Typography variant="h3" component="h1">
-                                {author.firstName + ' ' + author.lastName}
-                            </Typography>
-                        </Box>
-                        <Box pt={1}>
-                            <Typography variant="h6">
-                                {author.birthDate} - {author.deathDate},{' '}
-                                <Link href="/library/timeperiods/[id]" as={`/library/timeperiods/${author.timePeriod?._id}`}>
-                                    <a>
-                                        <SecondaryLink variant="inherit">
-                                            {author.timePeriod?.name}
-                                        </SecondaryLink>
-                                    </a>
-                                </Link>
-                            </Typography>
-                        </Box>
-                        <Divider />
-                        {favorites && <>
-                            <Box py={2}>
-                                {isFavorite ? <div>
-                                    <GoldSecondaryButton disabled={loading} onClick={() => changeFavStatus(false)}>
-                                        Remove from Favorites
-                                    </GoldSecondaryButton>
-                                </div> : <GoldSecondaryButton disabled={loading} onClick={() => changeFavStatus(true)}>
-                                    Add to Favorites
-                                </GoldSecondaryButton>}
+            <div className={styles['general-info']}>
+                <div className={styles['img-container']}>
+                    <img src={author.image || '/no-profile.jpg'} title={author.firstName + ' ' + author.lastName} />
+                    {favorites && isFavorite && <div className={styles.star}> 
+                        <StarIcon fontSize="large" />
+                    </div>}
+                </div>
+                <div className={styles['content-container']}>
+                    <Paper elevation={3}>
+                        <Box px={3}>
+                            <Box>
+                                <Typography variant="h3" component="h1">
+                                    {author.firstName + ' ' + author.lastName}
+                                </Typography>
+                            </Box>
+                            <Box pt={1}>
+                                <Typography variant="h6">
+                                    {author.birthDate} - {author.deathDate},{' '}
+                                    <Link href="/library/timeperiods/[id]" as={`/library/timeperiods/${author.timePeriod?._id}`}>
+                                        <a>
+                                            <SecondaryLink variant="inherit">
+                                                {author.timePeriod?.name}
+                                            </SecondaryLink>
+                                        </a>
+                                    </Link>
+                                </Typography>
                             </Box>
                             <Divider />
-                        </>}
-                        <Box pt={2}>
-                            <div className={styles['author-grid-container']}>
-                                <div>
-                                    <Box pl={3} pb={1}>
-                                        <Typography variant="h5">
-                                            Key Points
-                                        </Typography>
-                                    </Box>
-                                    <Box>
-                                        <BulletList>
-                                            {author.keyPoints.map((point, i) => (
-                                                <li key={i}>
-                                                    <Typography style={{maxWidth: '50ch'}} variant="body1">
-                                                        {point}
-                                                    </Typography>
-                                                </li>
-                                            ))}
-                                        </BulletList>
-                                    </Box>
-                                </div>
-                                <div>
-                                    <Box pl={3} pb={1}>
-                                        <Typography variant="h5">
-                                            Relevant Works
-                                        </Typography>
-                                    </Box>
-                                    <Box>
-                                        <BulletList>
-                                            {author.relevantWorks.map((work, i) => {
-                                                if(work.link) {
-                                                    return (
-                                                        <li key={i}>
-                                                            <a href={work.link}>
-                                                                <SecondaryLink variant="body1">
-                                                                    {work.name}
-                                                                </SecondaryLink>
-                                                            </a>
-                                                        </li>
-                                                    )
-                                                }
-                                                return (
+                            {favorites && <>
+                                <Box py={2}>
+                                    {isFavorite ? <div>
+                                        <GoldSecondaryButton disabled={loading} onClick={() => changeFavStatus(false)}>
+                                            Remove from Favorites
+                                        </GoldSecondaryButton>
+                                    </div> : <GoldSecondaryButton disabled={loading} onClick={() => changeFavStatus(true)}>
+                                        Add to Favorites
+                                    </GoldSecondaryButton>}
+                                </Box>
+                                <Divider />
+                            </>}
+                            <Box pt={2}>
+                                <div className={styles['author-grid-container']}>
+                                    <div>
+                                        <Box pl={3} pb={1}>
+                                            <Typography variant="h5">
+                                                Key Points
+                                            </Typography>
+                                        </Box>
+                                        <Box>
+                                            <BulletList>
+                                                {author.keyPoints.map((point, i) => (
                                                     <li key={i}>
-                                                        <Typography variant="body1">
-                                                            {work.name}
+                                                        <Typography style={{maxWidth: '50ch'}} variant="body1">
+                                                            {point}
                                                         </Typography>
                                                     </li>
-                                                )
-                                            })}
-                                        </BulletList>
-                                    </Box>
+                                                ))}
+                                            </BulletList>
+                                        </Box>
+                                    </div>
+                                    <div>
+                                        <Box pl={3} pb={1}>
+                                            <Typography variant="h5">
+                                                Relevant Works
+                                            </Typography>
+                                        </Box>
+                                        <Box>
+                                            <BulletList>
+                                                {author.relevantWorks.map((work, i) => {
+                                                    if(work.link) {
+                                                        return (
+                                                            <li key={i}>
+                                                                <a href={work.link}>
+                                                                    <SecondaryLink variant="body1">
+                                                                        {work.name}
+                                                                    </SecondaryLink>
+                                                                </a>
+                                                            </li>
+                                                        )
+                                                    }
+                                                    return (
+                                                        <li key={i}>
+                                                            <Typography variant="body1">
+                                                                {work.name}
+                                                            </Typography>
+                                                        </li>
+                                                    )
+                                                })}
+                                            </BulletList>
+                                        </Box>
+                                    </div>
                                 </div>
-                            </div>
+                            </Box>
                         </Box>
-                    </Box>
-                </Paper>
+                    </Paper>
+                </div>
             </div>
+            {premiumInfo && <Box mt={3}>
+                <div className={styles['detailed-info']}>
+                    <Paper elevation={3}>
+                        <Box px={3} py={3}>
+                            <Box className={styles['title-container']} mb={2} ml={2}>
+                                <Typography variant="h4">
+                                    Detailed Info
+                                </Typography>
+                            </Box>
+                            <Box ml={2} maxWidth={600}>
+                                <TextDisplay text={premiumInfo.detailedInfo} />
+                            </Box>
+                        </Box>
+                    </Paper>
+                </div>
+            </Box>}
         </>
     )
 }
