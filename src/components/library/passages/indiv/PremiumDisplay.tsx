8f4b3917e606369dccb36ef767a9f93b793a30e2
Manuel <manueljoseph113@gmail.com>
2021-06-01 10:06:10 -0400

    added pdf iframe and commentary section

--- src/components/library/passages/indiv/PremiumDisplay.tsx
+++ src/components/library/passages/indiv/PremiumDisplay.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import styles from '../../../../styles/Resource.module.css'
 import {Paper, Box, Typography} from '@material-ui/core'
+import TextDisplay from '../../../mui-rte/TextDisplay'
 
-export default function PremiumDisplay() {
+interface Props {
+    info: {
+        annotations:string;
+        commentary:string;
+    }
+}
+
+export default function PremiumDisplay({info}:Props) {
+
+    if(!info) return <div />
 
     return (
-        <div className={styles['no-premium-message-root']}>
-            <Paper elevation={3}>
-                <Box p={3} textAlign="center">
-                    <Typography variant="body1">
-                        Analysis, audio, and annotations coming soon with Spanish Bites Premium!
-                    </Typography>
-                </Box>
-            </Paper>
-        </div>
+        <>
+            <div style={{width: 'clamp(300px, 100%, 90ch)', minHeight: 500}}>
+                <iframe src={info.annotations} style={{height: '100%', width: '100%'}}></iframe>
+            </div>
+            <div className={styles['detailed-info']}>
+                    <Paper elevation={3}>
+                        <Box px={3} py={3}>
+                            <Box className={styles['title-container']} mb={2} ml={2}>
+                                <Typography variant="h4">
+                                    Commentary
+                                </Typography>
+                            </Box>
+                            <Box ml={2} maxWidth={600}>
+                                <TextDisplay text={info.commentary} />
+                            </Box>
+                        </Box>
+                    </Paper>
+                </div>
+        </>
     )
 }
