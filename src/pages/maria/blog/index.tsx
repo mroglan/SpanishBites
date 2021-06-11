08319b24ffa44f4a7261c364f16a60b1d0a27e2d
Manuel <manueljoseph113@gmail.com>
2021-06-11 19:59:35 -0400

    added index page

---
+++ src/pages/maria/blog/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Head from 'next/head'
+import { getInitialRecentBlogPosts } from '../../../utils/blog'
+import {GetStaticProps} from 'next'
+import useSWR from 'swr'
+import styles from '../../../styles/Basic.module.css'
+import MainHeader from '../../../components/nav/MainHeader'
+import MainFooter from '../../../components/nav/MainFooter'
+import Main from '../../../components/maria/blog/indexPage/Main'
+
+export default function Blog({posts}) {
+
+    console.log(posts)
+
+    const {data:user} = useSWR('/api/auth/getuser', {shouldRetryOnError: false})
+
+    return (
+        <>
+            <Head>
+                <title>Maria Carmen Blog</title>
+            </Head>
+            <div className={styles.root}>
+                <div className={styles.header}>
+                    <MainHeader bg="none" user={user} />
+                </div>
+                <div className={styles.main}>
+                    <Main posts={posts} />
+                </div>
+                <div className={styles.footer}>
+                    <MainFooter />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const getStaticProps:GetStaticProps = async () => {
+
+    const posts = await getInitialRecentBlogPosts()
+
+    return {props: {posts: JSON.parse(JSON.stringify(posts))}}
+}
