02ca7303ea25c5757601376a6d4d76f402a15ff2
Manuel <manueljoseph113@gmail.com>
2021-07-10 11:25:38 +0200

    added skeleton

--- src/pages/premium/index.tsx
+++ src/pages/premium/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import MainHeader from '../../components/nav/MainHeader'
+import MainFooter from '../../components/nav/MainFooter'
+import styles from '../../styles/Basic.module.css'
+import Head from 'next/head'
+import useSWR from 'swr'
+import Main from '../../components/premium/indexPage/Main'
 
 export default function Premium() {
 
+    const {data:user} = useSWR('/api/auth/getuser', {shouldRetryOnError: false})
+
     return (
-        <div>
-            premium page
-        </div>
+        <>
+            <Head>
+                <title>Spanish Bites Premium</title>
+            </Head>
+            <div className={styles.root}>
+                <div className={styles.header} style={{position: 'sticky', top: 0, zIndex: 2000}}>
+                    <MainHeader bg="none" user={user} />
+                </div>
+                <div className={styles.main}>
+                    <Main />
+                </div>
+                <div className={styles.footer}>
+                    <MainFooter />
+                </div>
+            </div>
+        </>
     )
 }
