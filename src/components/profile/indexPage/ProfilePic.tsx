006bd58f73ae31a9daaab0d4e8c2219088c62d39
Manuel <manueljoseph113@gmail.com>
2021-07-14 10:06:14 +0200

    added loading screen

--- src/components/profile/indexPage/ProfilePic.tsx
+++ src/components/profile/indexPage/ProfilePic.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useRef} from 'react'
 import styles from '../../../styles/Profile.module.css'
-import {Box, Avatar} from '@material-ui/core'
+import {Box, Avatar, CircularProgress} from '@material-ui/core'
 import { uploadImage } from '../../../utils/images'
 import { ClientCookieUser } from '../../../database/dbInterfaces'
 import {BlueDenseButton} from '../../items/buttons'
@@ -15,10 +15,14 @@ export default function ProfilePic({user}:Props) {
 
     const [imgSrc, setImgSrc] = useState(user.image?.src)
 
+    const [loading, setLoading] = useState(false)
+
     const handleFileUpload = async (e) => {
+        setLoading(true)
         const file = e.target.files[0]
         const image = await uploadImage(file)
         setImgSrc(image.src)
+        setLoading(false)
     }
 
     return (
@@ -26,6 +30,9 @@ export default function ProfilePic({user}:Props) {
             <Box maxWidth={400}>
                 <Box className={styles['pic-container']}>
                     <Avatar src={imgSrc || '/no-profile.jpg'} className={styles['pic-avatar']} />
+                    {loading && <div className={styles['pic-loading']}>
+                        <CircularProgress size={60} thickness={4} />
+                    </div>}
                 </Box>
                 <Box textAlign="center" mt={2}>
                     <input ref={imgInputRef} className={styles['pic-input']} type="file" onChange={(e) => handleFileUpload(e)}
