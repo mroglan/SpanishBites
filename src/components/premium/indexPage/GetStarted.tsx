3e0739a21bf7418a449e4a4cf2c3584f7107076d
Manuel <manueljoseph113@gmail.com>
2021-07-19 09:26:17 +0200

    added link

--- src/components/premium/indexPage/GetStarted.tsx
+++ src/components/premium/indexPage/GetStarted.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Box, Typography} from '@material-ui/core'
 import {BluePrimaryButton} from '../../items/buttons'
+import Link from 'next/link'
 
 export default function GetStarted() {
 
@@ -8,16 +9,20 @@ export default function GetStarted() {
         <div>
             <Box mx={3} mt={5} mb={3}>
                 <Box mx="auto"  width="clamp(100px, 100%, 500px)">
-                    <BluePrimaryButton fullWidth>
-                        <Box>
-                            <Typography variant="h6" gutterBottom>
-                                Get Started
-                            </Typography>
-                            <Typography variant="body1">
-                                $29.99 for 1 year
-                            </Typography>
-                        </Box>
-                    </BluePrimaryButton>
+                    <Link href="/premium/purchase">
+                        <a>
+                            <BluePrimaryButton fullWidth>
+                                <Box>
+                                    <Typography variant="h6" gutterBottom>
+                                        Get Started
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        $29.99 for 1 year
+                                    </Typography>
+                                </Box>
+                            </BluePrimaryButton>
+                        </a>
+                    </Link>
                 </Box>
             </Box>
         </div>
