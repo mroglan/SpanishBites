93d473ecceeff72146e1ea72233c771c8c6aa75e
Manuel <manueljoseph113@gmail.com>
2021-07-20 14:06:05 +0200

    added stripe_publishable_key

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
         DATABASE_PASS: process.env.DATABASE_PASS,
         BASE_URL: process.env.BASE_URL,
         SIGNATURE: process.env.SIGNATURE,
-        CLOUDINARY_NAME: process.env.CLOUDINARY_NAME
+        CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+        STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
     }
 }
