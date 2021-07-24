8a9e63d3ba7aab8382b863bbcc83daeb15745317
Manuel <manueljoseph113@gmail.com>
2021-07-24 16:21:31 +0200

    added getCustomerId and getPaymentIntent

---
+++ src/utils/payments.ts
@@ -0,0 +1,39 @@
+import React from 'react'
+import {ClientCookieUser, OrganizedDBUser} from '../database/dbInterfaces'
+import Stripe from 'stripe'
+import {addCustomerId} from './users'
+
+export const getCustomerId = async (stripe:Stripe, user:OrganizedDBUser) => {
+
+    if(user.customerId) {
+        return user.customerId
+    }
+
+    const customer = await stripe.customers.create({
+        email: user.email,
+        name: user.name,
+    })
+    
+    await addCustomerId(user._id, customer.id)
+    
+    return customer.id
+}
+
+export async function getPaymentIntnet(stripe:Stripe, user:OrganizedDBUser) {
+
+    const cusId = await getCustomerId(stripe, user)
+
+    const paymentIntents = await stripe.paymentIntents.list({
+        customer: cusId
+    })
+
+    if(paymentIntents.data.length && paymentIntents.data[0].status === 'requires_payment_method') {
+        return paymentIntents.data[0]
+    }
+
+    return await stripe.paymentIntents.create({
+        amount: 2999,
+        currency: 'USD',
+        customer: cusId
+    })
+}
