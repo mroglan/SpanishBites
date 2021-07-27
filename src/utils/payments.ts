90fa8be23d11637d265fcacdfff633ad86c7436f
Manuel <manueljoseph113@gmail.com>
2021-07-26 20:41:03 -0400

    added processPayment

--- src/utils/payments.ts
+++ src/utils/payments.ts
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {Dispatch, SetStateAction} from 'react'
 import {ClientCookieUser, OrganizedDBUser} from '../database/dbInterfaces'
 import Stripe from 'stripe'
+import {StripeElements} from '@stripe/stripe-js'
 import {addCustomerId} from './users'
+import axios from 'axios'
+import {CardElement} from '@stripe/react-stripe-js'
 
 export const getCustomerId = async (stripe:Stripe, user:OrganizedDBUser) => {
 
@@ -48,4 +51,55 @@ export async function getPaymentIntents(stripe:Stripe, user:OrganizedDBUser) {
     return await stripe.paymentIntents.list({
         customer: user.customerId
     })
+}
+
+interface ProcessPaymentStates{
+    setPayError: Dispatch<SetStateAction<boolean>>;
+    setPremiumError: Dispatch<SetStateAction<boolean>>;
+    setSuccess: Dispatch<SetStateAction<boolean>>;
+}
+
+interface ProcessPaymentBase {
+    stripe: any;
+    elements: StripeElements;
+    paymentIntent: Stripe.PaymentIntent;
+}
+
+interface ProcessPaymentVals {
+    name: string;
+    email: string;
+}
+
+export async function processPayment({stripe, elements, paymentIntent}:ProcessPaymentBase, vals:ProcessPaymentVals,
+     type:string, states:ProcessPaymentStates) {
+
+        const {error, paymentIntent:resPaymentIntent} = await stripe.confirmCardPayment(paymentIntent.client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: vals.name,
+                    email: vals.email
+                }
+            },
+        })
+
+        if(error) {
+            states.setPayError(true)
+            return
+        }
+
+        try {
+            
+            await axios({
+                method: 'POST',
+                url: `/api/premium/${type}`,
+                data: {paymentIntentId: resPaymentIntent.id }
+            })
+            
+        } catch(e) {
+            states.setPremiumError(true)
+            return
+        }
+
+        states.setSuccess(true)
 }
