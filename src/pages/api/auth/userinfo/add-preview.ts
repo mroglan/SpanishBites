4e0f9a549d8d63b7f93ffc9dfc5af31953667540
Manuel <manueljoseph113@gmail.com>
2021-07-31 18:42:31 -0400

    added add-preview

---
+++ src/pages/api/auth/userinfo/add-preview.ts
@@ -0,0 +1,36 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import {verifyUser} from '../../../../utils/auth'
+import {addPreview} from '../../../../utils/users'
+import jwt from 'jsonwebtoken'
+import {setCookie} from 'nookies'
+
+export default verifyUser(async function AddPreview(req:NextApiRequest, res:NextApiResponse) {
+
+    if(req.method !== 'POST') {
+        return res.status(400).json({msg: 'bye'})
+    }
+
+    try {
+
+        const itemInfo = {type: req.body.type, id: req.body.id}
+
+        await addPreview(req.body.jwtUser, itemInfo)
+
+        // make sure to also update the auth cookie
+
+        const claims = {...req.body.jwtUser, previews: [...req.body.jwtUser.previews, itemInfo]}
+
+        const token = jwt.sign(claims, process.env.SIGNATURE)
+
+        setCookie({res}, 'auth', token, {
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: true,
+            maxAge: 172800,
+            path: '/'
+        })
+
+        return res.status(200).json({msg: 'successful update'})
+    } catch(e) {
+        return res.status(500).json({msg: 'Internal server error'})
+    }
+})
