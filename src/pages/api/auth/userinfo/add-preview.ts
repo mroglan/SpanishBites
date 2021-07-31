f6e45bf3cb4b157d5c90a3ec0da6a0b10bce658d
Manuel <manueljoseph113@gmail.com>
2021-07-31 18:48:53 -0400

    checked for max # of previews

--- src/pages/api/auth/userinfo/add-preview.ts
+++ src/pages/api/auth/userinfo/add-preview.ts
@@ -12,6 +12,10 @@ export default verifyUser(async function AddPreview(req:NextApiRequest, res:Next
 
     try {
 
+        if(req.body.jwtUser.previews.length > 4) {
+            return res.status(400).json({msg: 'Exceeded preview limit'})
+        }
+
         const itemInfo = {type: req.body.type, id: req.body.id}
 
         await addPreview(req.body.jwtUser, itemInfo)
